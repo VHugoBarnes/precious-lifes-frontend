bc979a9ced9fd2e822aa1d7e50e025c28307f61e
Víctor Hugo Vázquez Gómez <hugovg799@gmail.com>
2021-04-19 23:48:29 -0500

    Header styles

---
+++ src/components/utils/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <h1 class="logo logo-main">Precious Lifes</h1>
+    <nav class="menu">
+      <ul>
+        <li>
+          <a href="#" class="link">quiénes somos</a>
+        </li>
+        <li>
+          <a href="#" class="link">cómo donar</a>
+        </li>
+        <li>
+          <a href="#" class="link">registrate</a>
+        </li>
+        <li>
+          <a href="#" class="link">iniciar sesión</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
